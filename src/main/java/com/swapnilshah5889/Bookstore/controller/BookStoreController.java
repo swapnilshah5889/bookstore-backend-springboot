f240508ef16f56547a4262e368e4f32ec4f0fcc6
Swapnil Shah <swapnilshah5889@gmail.com>
2024-03-23 21:54:48 -0400

    Added jdbc dependency. Added dev application.properties for jdbc connection

--- src/main/java/com/swapnilshah5889/Bookstore/controller/BookStoreController.java
+++ src/main/java/com/swapnilshah5889/Bookstore/controller/BookStoreController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/bookstore")
 public class BookStoreController {
-
+    
     @GetMapping("/healthcheck")
     public String healthCheck(){
         return "UP";
